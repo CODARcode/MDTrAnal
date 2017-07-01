64fb580c705445304f2387928c430fcb23089732
Shinjae Yoo <shinjae@gmail.com>
2017-07-01 12:15:21 -0400

    Added change distribution based sampling code

--- src/quick_dist.m
+++ src/quick_dist.m
@@ -2,6 +2,8 @@
 % step size for timestamp
 step = 100;
 load data/nt-ice_md-normal_new.mat
+% sampling rate 
+sampling_rate = 0.1; % 10%
 % reformat data
 [x, y, z] = size(trace);
 t2 = reshape(trace, [x, y*z]);
@@ -21,4 +23,32 @@ hold on
 for i =1:step:x
     text(c(i,1),c(i,2),c(i,3), int2str(i))
 end
+
+%% project to low dimensions
+n_dim = 2;
+ps = c(:,1:n_dim) * s(1:n_dim, 1:n_dim);
+%% visualize changes
+psd = ps(1:(x-1),1:n_dim) - ps(2:x, 1:n_dim);
+psdu = sum(abs(psd'));
+prob_dist = (psdu / sum(psdu));
+figure, plot(1:(x-1), sum(abs(psd),2));
+
+
+%% Sub-sampling
+total = 10^-10;
+sampling_entries = int32(x * sampling_rate);
+target = zeros(sampling_entries, y, z);
+t_idx = 1;
+for i=1:(x-1)
+    total = total + prob_dist(i);
+    if(total > 1 / double(sampling_entries))
+        target(t_idx,:,:) = trace(i,:,:);
+        total = total - 1 / double(sampling_entries);
+        t_idx = t_idx + 1;
+    end
+end
+% TODO: post processing of trimming may be required as t_idx may be ended
+% earlier due to one entry's prob_dist(i) can be 2 or more times than 1 /
+% sampling_entries
+
 
